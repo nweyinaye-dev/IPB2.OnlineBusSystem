cc96a03bc89869ccc94f98282ca0b22e34967a57
nweyinaye-11051997 <nweyinaye19970511@gmail.com>
2026-03-19 23:36:35 -0700

    feat: implementation minimum api

--- IPB2.OnlineBusSystem.MiniApi/Program.cs
+++ IPB2.OnlineBusSystem.MiniApi/Program.cs
@@ -1,12 +1,22 @@
 using IPB2.OnlineBusSystem.WebApi.Common;
 using IPB2.OnlineBusSystem.WebApi.Features.Admin.Bus;
+using IPB2.OnlineBusSystem.WebApi.Features.Admin.Route;
+using IPB2.OnlineScheduleSystem.WebApi.Features.Admin.Schedule;
+using IPB2.OnlineBusSystem.WebApi.Features.Report;
+using IPB2.OnlineBusSystem.WebApi.Features.User;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register Services
+builder.Services.AddScoped<BusService>();
+builder.Services.AddScoped<RouteService>();
+builder.Services.AddScoped<ScheduleService>();
+builder.Services.AddScoped<BookingReportService>();
+builder.Services.AddScoped<BookingService>();
 
 var app = builder.Build();
 
@@ -18,26 +28,213 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-# region bus controller
-var busApi = app.MapGroup("/api/bus").WithTags("Bus Endpoints");
 
-// GET: /api/teachers/{pageNo}/{pageSize}
-//busApi.MapGet("/{pageNo:int}/{pageSize:int}", async (int pageNo, int pageSize,BusService _busService) =>
-//{
-//    if (pageNo < 0) return Results.BadRequest(new ResponseBaseModel { IsSuccess = false, Message = "Invalid page number." });
-//    if (pageSize < 0) return Results.BadRequest(new ResponseBaseModel { IsSuccess = false, Message = "Invalid page size." });
+#region Bus Endpoints
+var busApi = app.MapGroup("/api/bus").WithTags("Bus");
 
-//    var result = await _busService.GetBusAsync(pageNo, pageSize);
-//    string message = result.Buss.Count > 0 ? "Get all Bus successfully." : "No data.";
+busApi.MapGet("/", async (int pageNo, int pageSize, BusService service) =>
+{
+    var response = await service.GetBusAsync(pageNo, pageSize);
+    return Results.Ok(response);
+});
 
-//    return Results.Ok(new GetAllTeacherResponse
-//    {
-//        IsSuccess = true,
-//        Message = message,
-//        data = result
-//    });
-//});
-# endregion
+busApi.MapGet("/search", async (string str, BusService service) =>
+{
+    var response = await service.GetBusesBySearchAsync(str);
+    return response == null 
+        ? Results.NotFound(new ResponseBaseModel { IsSuccess = false, Message = "Bus not found." }) 
+        : Results.Ok(response);
+});
+
+busApi.MapGet("/{id}", async (string id, BusService service) =>
+{
+    var response = await service.GetBusByIdAsync(id);
+    return response == null 
+        ? Results.NotFound(new ResponseBaseModel { IsSuccess = false, Message = "Bus not found." }) 
+        : Results.Ok(response);
+});
+
+busApi.MapPost("/", async (UpsertBusRequest request, BusService service) =>
+{
+    var validationRes = ValidateBus(request);
+    if (!validationRes.IsSuccess) return Results.BadRequest(validationRes);
+
+    var response = await service.CreateAsync(request);
+    return Results.Ok(response);
+});
+
+busApi.MapPut("/{id}", async (string id, UpsertBusRequest request, BusService service) =>
+{
+    var validationRes = ValidateBus(request);
+    if (!validationRes.IsSuccess) return Results.BadRequest(validationRes);
+
+    var response = await service.UpsertAsync(request, id);
+    return Results.Ok(response);
+});
+
+busApi.MapPatch("/{id}", async (string id, UpdateBusRequest request, BusService service) =>
+{
+    var response = await service.UpdateAsync(request, id);
+    return Results.Ok(response);
+});
+
+busApi.MapDelete("/{id}", async (string id, BusService service) =>
+{
+    var response = await service.DeleteAsync(id);
+    return Results.Ok(response);
+});
+
+ResponseBaseModel ValidateBus(UpsertBusRequest request)
+{
+    if (string.IsNullOrWhiteSpace(request.BusName)) return new ResponseBaseModel { IsSuccess = false, Message = "BusName is required." };
+    if (string.IsNullOrWhiteSpace(request.BusNo)) return new ResponseBaseModel { IsSuccess = false, Message = "BusNo is required." };
+    if (string.IsNullOrWhiteSpace(request.BusType)) return new ResponseBaseModel { IsSuccess = false, Message = "BusType is required." };
+    if (request.TotalSeat < 20) return new ResponseBaseModel { IsSuccess = false, Message = "Total Seat must be at leave 20 seats." };
+    return new ResponseBaseModel { IsSuccess = true };
+}
+#endregion
+
+#region Route Endpoints
+var routeApi = app.MapGroup("/api/routes").WithTags("Route");
+
+routeApi.MapGet("/", async (int pageNo, int pageSize, RouteService service) =>
+{
+    var response = await service.GetRoutesAsync(pageNo, pageSize);
+    return Results.Ok(response);
+});
+
+routeApi.MapGet("/{id}", async (string id, RouteService service) =>
+{
+    var response = await service.GetRouteByIdAsync(id);
+    return response == null 
+        ? Results.NotFound(new ResponseBaseModel { IsSuccess = false, Message = "Route not found." }) 
+        : Results.Ok(response);
+});
+
+routeApi.MapPost("/", async (UpsertRouteRequest request, RouteService service) =>
+{
+    var validationRes = ValidateRoute(request);
+    if (!validationRes.IsSuccess) return Results.BadRequest(validationRes);
+
+    var response = await service.CreateAsync(request);
+    return Results.Ok(response);
+});
+
+routeApi.MapPut("/{id}", async (string id, UpsertRouteRequest request, RouteService service) =>
+{
+    var validationRes = ValidateRoute(request);
+    if (!validationRes.IsSuccess) return Results.BadRequest(validationRes);
+
+    var response = await service.UpsertAsync(request, id);
+    return Results.Ok(response);
+});
+
+routeApi.MapPatch("/{id}", async (string id, UpdateRouteRequest request, RouteService service) =>
+{
+    var response = await service.UpdateAsync(request, id);
+    return Results.Ok(response);
+});
+
+routeApi.MapDelete("/{id}", async (string id, RouteService service) =>
+{
+    var response = await service.DeleteAsync(id);
+    return Results.Ok(response);
+});
+
+ResponseBaseModel ValidateRoute(UpsertRouteRequest request)
+{
+    if (string.IsNullOrWhiteSpace(request.RouteName)) return new ResponseBaseModel { IsSuccess = false, Message = "Route name is required." };
+    if (string.IsNullOrWhiteSpace(request.Origin)) return new ResponseBaseModel { IsSuccess = false, Message = "Origin no is required." };
+    if (string.IsNullOrWhiteSpace(request.Destination)) return new ResponseBaseModel { IsSuccess = false, Message = "Destination is required." };
+    return new ResponseBaseModel { IsSuccess = true };
+}
+#endregion
+
+#region Schedule Endpoints
+var scheduleApi = app.MapGroup("/api/schedules").WithTags("Schedule");
+
+scheduleApi.MapGet("/", async (int pageNo, int pageSize, ScheduleService service) =>
+{
+    var response = await service.GetScheduleAsync(pageNo, pageSize);
+    return Results.Ok(response);
+});
+
+scheduleApi.MapGet("/{id}", async (string id, ScheduleService service) =>
+{
+    var response = await service.GetScheduleByIdAsync(id);
+    return response == null 
+        ? Results.NotFound(new ResponseBaseModel { IsSuccess = false, Message = "Schedule not found." }) 
+        : Results.Ok(response);
+});
+
+scheduleApi.MapPost("/", async (UpsertScheduleRequest request, ScheduleService service) =>
+{
+    var validationRes = ValidateSchedule(request);
+    if (!validationRes.IsSuccess) return Results.BadRequest(validationRes);
+
+    var response = await service.CreateAsync(request);
+    return Results.Ok(response);
+});
+
+scheduleApi.MapPut("/{id}", async (string id, UpsertScheduleRequest request, ScheduleService service) =>
+{
+    var validationRes = ValidateSchedule(request);
+    if (!validationRes.IsSuccess) return Results.BadRequest(validationRes);
+
+    var response = await service.UpsertAsync(request, id);
+    return Results.Ok(response);
+});
+
+scheduleApi.MapPatch("/{id}", async (string id, UpdateScheduleRequest request, ScheduleService service) =>
+{
+    var response = await service.UpdateAsync(request, id);
+    return Results.Ok(response);
+});
+
+scheduleApi.MapDelete("/{id}", async (string id, ScheduleService service) =>
+{
+    var response = await service.DeleteAsync(id);
+    return Results.Ok(response);
+});
+
+ResponseBaseModel ValidateSchedule(UpsertScheduleRequest request)
+{
+    if (string.IsNullOrWhiteSpace(request.BusId)) return new ResponseBaseModel { IsSuccess = false, Message = "BusId is required." };
+    if (request.Fare == 0) return new ResponseBaseModel { IsSuccess = false, Message = "Fare is required." };
+    if (string.IsNullOrWhiteSpace(request.ArrivalTime)) return new ResponseBaseModel { IsSuccess = false, Message = "ArrivalTime is required." };
+    if (string.IsNullOrWhiteSpace(request.DepartureTime)) return new ResponseBaseModel { IsSuccess = false, Message = "DepartureTime is required." };
+    if (string.IsNullOrWhiteSpace(request.RouteId)) return new ResponseBaseModel { IsSuccess = false, Message = "RouteId is required." };
+    return new ResponseBaseModel { IsSuccess = true };
+}
+#endregion
+
+#region Booking Endpoints
+app.MapGet("/api/booking", async (BookingReportService service) =>
+{
+    var response = await service.GetBookingDetailAsync("");
+    return Results.Ok(response);
+}).WithTags("Booking Report");
+
+app.MapPost("/search", async (SearchBusRequest request, BookingService service) =>
+{
+    if (string.IsNullOrWhiteSpace(request.Origin)) return Results.BadRequest(new ResponseBaseModel { IsSuccess = false, Message = "Origin is required." });
+    if (string.IsNullOrWhiteSpace(request.Destination)) return Results.BadRequest(new ResponseBaseModel { IsSuccess = false, Message = "Destination is required." });
+
+    var response = await service.SearchBus(request);
+    return Results.Ok(response);
+}).WithTags("User Booking");
+
+app.MapPost("/book", async (BookRequest request, BookingService service) =>
+{
+    if (string.IsNullOrWhiteSpace(request.ScheduleId)) return Results.BadRequest(new ResponseBaseModel { IsSuccess = false, Message = "Schedule Id is required." });
+    if (request.Passengers == null || !request.Passengers.Any()) return Results.BadRequest(new ResponseBaseModel { IsSuccess = false, Message = "No passengers provided." });
+    if (request.Passengers.Any(p => p.SeatNo <= 0 || string.IsNullOrWhiteSpace(p.Username) || string.IsNullOrWhiteSpace(p.Phoneno)))
+        return Results.BadRequest(new ResponseBaseModel { IsSuccess = false, Message = "Passenger details (Seat, Name, Phone) are required." });
+
+    var response = await service.CreateAsync(request);
+    return Results.Ok(response);
+}).WithTags("User Booking");
+#endregion
 
 app.Run();
 
